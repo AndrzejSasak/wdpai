80947ae923db24d2b26f262b1bd55e6a59066628
AndrzejSasak <andrzej.sasak01@gmail.com>
2022-12-25 13:20:13 +0100

    Improve responsive look

--- public/views/outfit-part.php
+++ public/views/outfit-part.php
@@ -3,5 +3,6 @@
         <label class="outfit-part-label"><?= $category ?></label>
         <?php if(isset($outfitPart)) { ?>
             <img src="public/uploads/<?= $outfitPart->getImage() ?>" class="outfit-part-image">
+            <h2 class="clothing-name"><?= $outfitPart->getName()?></h2>
     <?php } } ?>
 </div>
